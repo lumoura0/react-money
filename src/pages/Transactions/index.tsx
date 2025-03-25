baefa2111dad11d5a46daad4979b8e1fef126885
Luis Fernando Moura Goncalves <lfmgoncalves@emsurb.se.gov.br>
2025-03-25 13:48:30 -0300

    Estrutura visual - Tabela de transações

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,11 +1,36 @@
 import { Header } from '../../components/Header/index';
 import { Summary } from '../../components/Summary';
+import { PriceHighLight, TransactionsContainer, TransactionsTable } from './style';
 
 export function Transactions() {
     return (
         <div>
             <Header />
             <Summary />
+
+            <TransactionsContainer>
+                <TransactionsTable>
+                    <tbody>
+                        <tr>
+                            <td width="40%">Desenvolvimento de site</td>
+                            <td>
+                                <PriceHighLight variant="income">R$ 12.000,00</PriceHighLight>
+                            </td>
+                            <td>Venda</td>
+                            <td>13/04/2022</td>
+                        </tr>
+                        <tr>
+                            <td width="40%">Coxinha</td>
+                            <td>
+                                <PriceHighLight variant="outcome">- R$ 5,00
+                                </PriceHighLight>
+                            </td>
+                            <td>Alimentação</td>
+                            <td>10/04/2022</td>
+                        </tr>
+                    </tbody>
+                </TransactionsTable>
+            </TransactionsContainer>
         </div>
     )
 }
