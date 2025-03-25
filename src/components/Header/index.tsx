01fc915593839cff24b9c8b97574b98ad03c7c3c
Luis Fernando Moura Goncalves <lfmgoncalves@emsurb.se.gov.br>
2025-03-25 13:33:53 -0300

    Estrutura visual - Componente: Summary

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,5 +1,5 @@
 import { HeaderContainer, HeaderContent, NewTransactionButton } from './styles';
-import logoImg from '../assets/logo.svg';
+import logoImg from '../../assets/logo.svg';
 export function Header() {
     return (
         <HeaderContainer>
--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,9 +1,11 @@
-import { Header } from '../../components/Header';
+import { Header } from '../../components/Header/index';
+import { Summary } from '../../components/Summary';
 
 export function Transactions() {
     return (
         <div>
             <Header />
+            <Summary />
         </div>
     )
 }
