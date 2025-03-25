5390bec92c5dc9c3d9e28d748e9228f32d115e83
Luis Fernando Moura Goncalves <lfmgoncalves@emsurb.se.gov.br>
2025-03-25 14:07:17 -0300

    Estrutura visual - Componente: SearchForm

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,5 +1,6 @@
 import { Header } from '../../components/Header/index';
 import { Summary } from '../../components/Summary';
+import { SearchForm } from './components/SearchForm';
 import { PriceHighLight, TransactionsContainer, TransactionsTable } from './style';
 
 export function Transactions() {
@@ -9,6 +10,7 @@ export function Transactions() {
             <Summary />
 
             <TransactionsContainer>
+                <SearchForm />
                 <TransactionsTable>
                     <tbody>
                         <tr>
