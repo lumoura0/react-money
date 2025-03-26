3307063a248a147af3d73964762a7fe2db30b067
Luis Fernando Moura Goncalves <lfmgoncalves@emsurb.se.gov.br>
2025-03-26 15:14:16 -0300

    Conexão com a API - Calculando resumo de transações

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -6,7 +6,22 @@ import { TransactionsContext } from '../../contexts/TransactionsContext';
 export function Summary() {
     const { transactions } = useContext(TransactionsContext)
 
-    console.log(transactions)
+    const summary = transactions.reduce((acc, transaction) => {
+        if (transaction.type === 'income') {
+            acc.income += transaction.price
+            acc.total += transaction.price
+        } else {
+            acc.outcome += transaction.price
+            acc.total -= transaction.price
+        }
+
+        return acc
+    }, {
+        income: 0,
+        outcome: 0,
+        total: 0
+    })
+
     return (
         <SummaryContainer>
             <SummaryCard>
@@ -15,7 +30,7 @@ export function Summary() {
                     <ArrowCircleUp size={32} color='#00b37e' />
                 </header>
 
-                <strong>R$ 17.400,00</strong>
+                <strong>{summary.income}</strong>
             </SummaryCard>
 
             <SummaryCard>
@@ -23,7 +38,7 @@ export function Summary() {
                     <span>Saídas</span>
                     <ArrowCircleDown size={32} color='#f75a68' />
                 </header>
-                <strong>R$ 17.400,00</strong>
+                <strong>{summary.outcome}</strong>
             </SummaryCard>
 
             <SummaryCard variant='green'>
@@ -31,7 +46,7 @@ export function Summary() {
                     <span>Total</span>
                     <CurrencyDollar size={32} color='#fff' />
                 </header>
-                <strong>R$ 17.400,00</strong>
+                <strong>{summary.total}</strong>
             </SummaryCard>
         </SummaryContainer>
     )
