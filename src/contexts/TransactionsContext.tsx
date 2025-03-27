05311d60d8e06feaa54b46bc0327be20d6c9467a
Luis Fernando <luisfernando.mourag@gmail.com>
2025-03-27 17:49:27 -0300

    Conexão com a API - Criando nova transação

--- src/contexts/TransactionsContext.tsx
+++ src/contexts/TransactionsContext.tsx
@@ -10,9 +10,17 @@ interface Transaction {
     createdAt: string;
 }
 
+interface CreateTransactionInput {
+    description: string;
+    price: number;
+    category: string;
+    type: 'income' | 'outcome';
+}
+
 interface TransactionContextType {
     transactions: Transaction[];
     fetchTransactions: (query?: string) => Promise<void>;
+    createTransaction: (data: CreateTransactionInput) => Promise<void>;
 }
 
 interface TransactionsProviderProps {
@@ -26,10 +34,28 @@ export function TransactionsProvider({ children }: TransactionsProviderProps) {
 
     async function fetchTransactions(query?: string) {
         const response = await api.get('/transactions', {
-            params: { q: query },
+            params: {
+                _sort: 'createdAt',
+                _order: 'desc',
+                q: query
+            },
         })
 
         setTransactions(response.data)
+    }
+
+    async function createTransaction(data: CreateTransactionInput) {
+        const { description, category, price, type } = data
+
+        const response = await api.post('transactions', {
+            description,
+            category,
+            price,
+            type,
+            createdAt: new Date(),
+        })
+
+        setTransactions(state => [response.data, ...state])
     }
 
     useEffect(() => {
@@ -40,6 +66,7 @@ export function TransactionsProvider({ children }: TransactionsProviderProps) {
         <TransactionsContext.Provider value={{
             transactions,
             fetchTransactions,
+            createTransaction
         }}>
             {children}
         </TransactionsContext.Provider>
